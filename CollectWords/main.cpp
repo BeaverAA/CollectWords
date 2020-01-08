9cd26b422beb3bb29c87653430449e5f265f8312
newboba <newboba@yandex-team.ru>
2020-01-08 17:07:05 +0300

    Initial Commit

---
+++ CollectWords/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CollectWords
+//
+//  Created by Anton Bobr on 1/8/20.
+//  Copyright © 2020 Anton Bobr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
